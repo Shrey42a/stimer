8c5335fd2f4a078946b0752106831387a20286f7
Shrey kumar <shreykumar55242@gmail.com>
2024-03-17 18:54:14 +0530

    updates

--- src/DateTimeDisplay.jsx
+++ src/DateTimeDisplay.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const DateTimeDisplay = ({ value, type, isDanger }) => {
   return (
-    <div className="card2 lg:px-5 px-3 hover:bg-zinc-800 py-2 lg:py-4">
-      <div className={isDanger ? 'countdown danger' : 'countdown'}>
-        <p>{value}</p>
-        <span className='mt-2'>{type}</span>
+    <div className="card2 cursor-pointer hover:bg-teal-500 lg:py-4 md:py-4 py-14 px-14 sm:px-4 md:px-8 lg:px-8 flex w-max items-center justify-center text-center">
+      <div id='jj' className={isDanger ? 'countdown danger' : 'countdown'}>
+        <p className='text-7xl lg:text-5xl'>{value}</p>
+        <span className='text-yellow-400'>{type}</span>
       </div>
     </div>
   );
--- src/ShowCounter.jsx
+++ src/ShowCounter.jsx
@@ -2,13 +2,7 @@ import DateTimeDisplay from "./DateTimeDisplay";
 
 export const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
-    <div className="show-counter w-full">
-      <a
-        href="https://tapasadhikary.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="countdown-link space-x-2 lk"
-      >
+    <div className="show-counter flex flex-col lg:flex-row md:flex-row  w-full">
         <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
         <p>:</p>
         <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
@@ -16,7 +10,6 @@ export const ShowCounter = ({ days, hours, minutes, seconds }) => {
         <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
         <p>:</p>
         <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-      </a>
     </div>
   );
 };
