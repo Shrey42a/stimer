4c9cdd59f35b19c03540791f2589bf493de816c3
Shrey kumar <shreykumar55242@gmail.com>
2024-03-17 19:58:11 +0530

    minor

--- src/DateTimeDisplay.jsx
+++ src/DateTimeDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DateTimeDisplay = ({ value, type, isDanger }) => {
   return (
-    <div className="card2 cursor-pointer hover:bg-teal-500 lg:py-4 md:py-4 py-14 px-14 sm:px-4 md:px-8 lg:px-8 flex w-max items-center justify-center text-center">
+    <div className="card2 cursor-pointer hover:bg-teal-500 lg:py-4 md:py-4 py-10 px-10 sm:px-4 md:px-8 lg:px-8 flex w-max items-center justify-center text-center">
       <div id='jj' className={isDanger ? 'countdown danger' : 'countdown'}>
         <p className='text-7xl lg:text-5xl'>{value}</p>
         <span className='text-yellow-400'>{type}</span>
